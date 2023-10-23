8f2afcd790b562d28487ee3d16a2d14bb0627d7d
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-10-23 15:09:20 +0900

    added template
    Added more template code for the test.

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
