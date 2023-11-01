a4e210cb9cf339d311e7ebd384f4b7732ccfe76d
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-11-01 15:06:38 +0900

    Added API
    Added the new api settings now. All looks well!

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,17 +1,23 @@
-import { useState} from 'react'
+import { useState} from 'react';
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator 
-} from 'react-native'
-import { useRouter } from 'expo-router'
-
-import styles from './popularjobs.style'
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import useFetch from '../../../hook/useFetch';
+import styles from './popularjobs.style';
 import { COLORS, SIZES } from '../../../constants';
 import PopularjobCard from '../../common/cards/popular/PopularJobCard';
 
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+
+  const { data, isLoading, error } = useFetch
+  ('search', {
+    query: 'React developer',
+    num_pages: 1
+  })
+
+  // console.log(data)
 
   return (
     <View style={styles.container}>
@@ -29,7 +35,7 @@ const Popularjobs = () => {
           <Text>Something when wrong</Text>
         ) :(
           <FlatList 
-          data={[1,2,3,4,5,6,7,8,9]}
+          data={data}
           renderItem={({ item }) => (
             <PopularjobCard
               item={item}
@@ -45,4 +51,4 @@ const Popularjobs = () => {
   )
 }
 
-export default Popularjobs
+export default Popularjobs;
