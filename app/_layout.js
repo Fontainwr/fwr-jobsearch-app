80ca05dfda90b6778cfcbf53f50a214711c8c8a2
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-11-06 17:22:22 +0900

    edit
    Made some changes to the code and site due to page not working.

--- app/_layout.js
+++ app/_layout.js
@@ -1,25 +1,29 @@
 import { Stack } from 'expo-router';
-import { useCallback } from 'react';
+// import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-const Layout = () => {
-    const [fontsLoaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-    })
-
-    const onLayoutRootView = useCallback(async () => {
-        if(fontsLoaded) {
-            await SplashScreen.hideAsync();
+export const unstable_settings = {
+    // Ensure any route can link back to `/`
+    initialRouteName: "home",
+    };
+    
+    const Layout = () => {
+        const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+        });
+    
+        if (!fontsLoaded) {
+        return null;
         }
-    }, [fontsLoaded])
-
-
-    if(!fontsLoaded) return null;
-
-    return <Stack onLayout={onLayoutRootView} />
-}
+    
+        return (
+        <Stack initialRouteName="home">
+            <Stack.Screen name="home" />
+        </Stack>
+        )
+};
 
 export default Layout;
