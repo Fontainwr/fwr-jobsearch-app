4d7785a165e7b5625ee6131af969fcee4a177148
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-11-06 17:10:50 +0900

    Added more
    Adde some changes to the app and will need to edit more later.

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,18 +1,17 @@
-import { View, Text, TouchableOpacity, ActivityIndicator 
-} from 'react-native';
-import { useRouter } from 'expo-router';
-import useFetch from '../../../hook/useFetch';
-import styles from './nearbyjobs.style';
-import { COLORS } from '../../../constants';
-import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
+import React from "react";
+import { useRouter } from "expo-router";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 
+import styles from "./nearbyjobs.style";
+import { COLORS } from "../../../constants";
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
+import useFetch from "../../../hook/useFetch";
 
-const NearbyJobs= () => {
+const Nearbyjobs = () => {
   const router = useRouter();
-  const { data, isLoading, error } = useFetch
-  ('search', {
-    query: 'React developer',
-    num_pages: 1
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Native developer",
+    num_pages: "1",
   });
 
   return (
@@ -20,27 +19,27 @@ const NearbyJobs= () => {
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Nearby jobs</Text>
         <TouchableOpacity>
-        <Text style={styles.header}>Show All</Text>
+          <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" colors={COLORS.primary} />
-        ) :error ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
           data?.map((job) => (
-            <NearbyJobCard 
+            <NearbyJobCard
               job={job}
-              key={`nearby-job-${job?.job_id}`}
+              key={`nearby-job-${job.job_id}`}
               handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
             />
           ))
         )}
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default NearbyJobs;
+export default Nearbyjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,71 +1,70 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
-  View, 
+  View,
   Text,
   TextInput,
   TouchableOpacity,
   Image,
-  FlatList
-} from 'react-native'
-import {useRouter } from 'expo-router';
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
-import styles from './welcome.style'
-import { icons, SIZES } from '../../../constants'
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const jobTypes = ["Full-time", "part-time", "contractor"];
-
-const Welcome = () => {
-  const router = useRouter(); 
-  const [activeJobType, setActiveJobType] = useState('Full-time')
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
 
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Fontain</Text>
-        <Text style={styles.welcomeMessage}>Find your perfect job good buddy!</Text>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput 
+          <TextInput
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
-            placeholder='What are you looking for today?'
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder='What are you looking for?'
           />
         </View>
-        
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
             resizeMode='contain'
             style={styles.searchBtnImage}
           />
-
         </TouchableOpacity>
       </View>
+
       <View style={styles.tabsContainer}>
-        <FlatList 
+        <FlatList
           data={jobTypes}
           renderItem={({ item }) => (
             <TouchableOpacity
               style={styles.tab(activeJobType, item)}
               onPress={() => {
                 setActiveJobType(item);
-                router.push(`/search/${item}`)
+                router.push(`/search/${item}`);
               }}
-              >
-                <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
-              keyExtractor={item => item}
-              contentContainerStyle={{ columnGap: SIZES.small}}
-              horizontal
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
