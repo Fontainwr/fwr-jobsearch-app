0ff34ff422728d6f7b21f15e0ef0cf94172b5e5b
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-11-08 20:05:25 +0900

    Update
    Updated the app

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { TouchableOpacity, Image } from 'react-native';
+import { Image, TouchableOpacity } from "react-native";
 
-import styles from './screenheader.style';
+import styles from "./screenheader.style";
 
 const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
   return (
     <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
-      <Image 
+      <Image
         source={iconUrl}
         resizeMode='cover'
-        style={styles.btnImg(dimension)} />
+        style={styles.btnImg(dimension)}
+      />
     </TouchableOpacity>
   );
 };
