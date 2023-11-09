7e0dbc8b10464400d336bf3fcb1e580693332cb5
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-11-09 15:10:56 +0900

    Finished
    Ready to start the deployment process now.

--- app/search/[id].js
+++ app/search/[id].js
@@ -26,7 +26,7 @@ const JobSearch = () => {
                 method: "GET",
                 url: `https://jsearch.p.rapidapi.com/search`,
                 headers: {
-                    "X-RapidAPI-Key": '',
+                    "X-RapidAPI-Key": '690b754741mshcc53829200709cap1286dcjsnec9f73c5cccb',
                     "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
                 },
                 params: {
